e1d7708e24c753375888d518e0bf6519814e5eae
Emily Richardson <richar35@mail.nmc.edu>
2020-09-21 19:17:10 -0400

    Add project files.

---
+++ MovieList/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieList.Models;
+
+namespace MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context { get; set; }
+
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+          public IActionResult Index()
+        {
+            var movies = context.Movies.OrderBy(m => m.Name).ToList();
+            return View();
+        }
+
+    }
+}
